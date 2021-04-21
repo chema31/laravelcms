facfb9e845cf66e16aa925172073f1d8ac2824d7
Chema <josemaria.delanieta@accenture.com>
2021-04-21 11:06:45 +0200

    Created users and roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,18 @@ class User extends Authenticatable
     public function pages() {
         return $this->hasMany('App\Models\Page');
     }
+
+    public function roles() {
+        return $this->belongsToMany('App\Models\Role');
+    }
+
+    public function hasAnyRole($roles)
+    {
+        return null != $this->roles()->whereIn('name', $roles)->first();
+    }
+
+    public function hasRole($role)
+    {
+        return null != $this->roles()->where('name', $role)->first();
+    }
 }
