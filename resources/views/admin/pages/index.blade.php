9c34e21488809e4de2e16351cd803b1a348fc5ac
Chema <josemaria.delanieta@accenture.com>
2021-04-30 11:28:11 +0200

    Styled administrator lists for pages, posts and users

--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="grid grid-flow-row auto-rows-max gap-6 p-6 bg-white border-b border-gray-200">
 
                     @if(session('status'))
                         <div>
@@ -16,33 +16,40 @@
                         </div>
                     @endif
 
-                    <a href="{{ route('pages.create') }}">Create new</a>
+                    <div>
+                        <a class="bg-indigo-400 hover:bg-indigo-700 text-white p-2 rounded-md" href="{{ route('pages.create') }}">Create new</a>
+                    </div>
 
-                    <table class="table-fixed border">
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>URL</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($pages as $page)
+                    <div>
+                        <table class="table-auto border border-collapse w-full">
+                            <thead>
                                 <tr>
-                                    <td>
-                                        <a href="{{ route('pages.edit', ['page' => $page->id]) }}">
-                                            {{ $page->present()->paddedTitle }}
-                                        </a>
-                                    </td>
-                                    <td>{{ $page->url }}</td>
+                                    <th class="bg-gray-200">Title</th>
+                                    <th class="bg-gray-200">URL</th>
+                                    <th class="bg-gray-200">Actions</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                        <tfoot>
-                        <tr>
-                            <td colspan="3">{{ $pages->links() }}</td>
-                        </tr>
-                        </tfoot>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($pages as $page)
+                                    <tr class="{{ $loop->even? 'bg-gray-100' : '' }}">
+                                        <td class="border p-2">{{ $page->present()->paddedTitle }}</td>
+                                        <td class="border p-2">{{ $page->url }}</td>
+                                        <td class="border p-2">
+                                            <a href="{{ route('pages.edit', ['page' => $page->id]) }}" class="text-indigo-400 hover:text-indigo-700">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 stroke-current" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                                </svg>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div>
+                        {{ $pages->links() }}
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="grid grid-flow-row auto-rows-max gap-6 p-6 bg-white border-b border-gray-200">
 
                     @if(session('status'))
                         <div>
@@ -16,33 +16,38 @@
                         </div>
                     @endif
 
-                    <table class="table-fixed border">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Roles</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($model as $user)
+                    <div>
+                        <table class="table-auto border border-collapse w-full">
+                            <thead>
                                 <tr>
-                                    <td>
-                                        <a href="{{ route('users.edit', ['user' => $user->id]) }}">
-                                            {{ $user->name }}
-                                        </a>
-                                    </td>
-                                    <td>{{ $user->email }}</td>
-                                    <td>{{ implode(', ', $user->roles()->pluck('name')->toArray()) }}</td>
+                                    <th class="bg-gray-200">Name</th>
+                                    <th class="bg-gray-200">Email</th>
+                                    <th class="bg-gray-200">Roles</th>
+                                    <th class="bg-gray-200">Actions</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <td colspan="3">{{ $model->links() }}</td>
-                            </tr>
-                        </tfoot>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($model as $user)
+                                    <tr class="{{ $loop->even? 'bg-gray-100' : '' }}">
+                                        <td class="border p-2">{{ $user->name }}</td>
+                                        <td class="border p-2">{{ $user->email }}</td>
+                                        <td class="border p-2">{{ implode(', ', $user->roles()->pluck('name')->toArray()) }}</td>
+                                        <td class="border p-2">
+                                            <a href="{{ route('users.edit', ['user' => $user->id]) }}" class="text-indigo-400 hover:text-indigo-700">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 stroke-current" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                                </svg>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div>
+                        {{ $model->links() }}
+                    </div>
                 </div>
             </div>
         </div>
